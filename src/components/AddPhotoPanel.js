f5a290b992fec5546e124698cf2caf88139da998
joshedge <joshedge04@gmail.com>
2022-01-25 22:08:25 -0800

    repositioned delete option to overlay on image, only appear when hovered over.

--- src/components/AddPhotoPanel.js
+++ src/components/AddPhotoPanel.js
@@ -21,9 +21,15 @@ const AddPhotoPanel = ({ handleAddPhoto }) => {
     <div className="flex flex-row justify-center w-[100%] items-center mb-8">
       <div className="">
         <input
-          className="border-solid rounded-md border-2 border-emerald-900 py-1 pl-4 mr-4 focus:outline-emerald-700"
+          className="border-solid rounded-md border-2 border-emerald-900 py-1 pl-4 mr-4 focus:outline-emerald-700 test-left"
           placeholder="Search for a photo..."
           value={photoText}
+          onKeyPress={(ev) => {
+            if (ev.key === "Enter") {
+              ev.preventDefault();
+              handleAddClick();
+            }
+          }}
           onChange={handleChange}
         ></input>
       </div>
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,12 +2,12 @@ import { MdOutlineDeleteForever } from "react-icons/md";
 
 const Photo = ({ id, url, alt, handleDeleteClick }) => {
   return (
-    <div className="mx-0 mb-0 mt-1">
+    <div className="relative mx-0 mb-0 mt-1">
       <img src={url} alt={alt} width="100%" />
       <MdOutlineDeleteForever
         onClick={() => handleDeleteClick(id)}
-        className="add-icon"
-        size="2.75rem"
+        className="delete-icon absolute right-0 bottom-0 opacity-0 hover:opacity-100"
+        size="2rem"
       />
     </div>
   );
